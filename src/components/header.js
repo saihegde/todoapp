8f6b1f0573d6887923109907e517a45ed79968c2
UTAH\shegde <shegde@utah.gov>
2018-03-29 16:21:06 -0600

    Styled the header

--- src/components/header.js
+++ src/components/header.js
@@ -13,10 +13,15 @@ const Header = () => {
 
 const styles = {
     viewStyle: {
-        backgroundColor: '#F8F8F8'
+        backgroundColor: '#ff0000',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15
     },
     textStyle: {
-        fontSize: 20
+        fontSize: 20,
+        color: '#ffffff'
     }
 }
 
